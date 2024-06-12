e9b19c455050f782fa21fb6f1518f8f898892f65
Ahmaddhr1 <ahmaddaherr004@gmail.com>
2024-06-12 22:45:39 +0300

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-2xl">Whereas</div>;
+}
+
+export default App;
---
+++ app/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0a060e',
+        'secondary': '#7245b2',
+      },
+      fontFamily: { 
+        'jose': ['Josefin Sans','sans-serif'],
+        'outfit': ['Outfit','sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
