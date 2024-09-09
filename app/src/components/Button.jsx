55d84882bf2aca356fa330fa5b11b4f5c4ff6cdc
Ahmad Daher <159710737+Ahmaddhr1@users.noreply.github.com>
2024-09-09 03:03:12 +0300

    Update Button.jsx

--- app/src/components/Button.jsx
+++ app/src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ text, icon }) => {
       variants={fadeInLeftDelayed}
       initial="initial"
       animate="animate"
-      href="/Ahmad Daher CV.docx"
+      href="/Ahmad Daher CV.pdf"
       donwnload="true"
       className="flex items-center justify-center gap-3 px-4 py-3 bg-secondary border-0 rounded-lg cursor-pointer "
     >
