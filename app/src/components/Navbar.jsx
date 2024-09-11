26078beb579bafd40bd9a2fddb5337fbc7c09293
Ahmad Daher <159710737+Ahmaddhr1@users.noreply.github.com>
2024-09-11 15:42:44 +0300

    Update Navbar.jsx

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const [isScrollingDown, setIsScrollingDown] = useState(false);
 
   useMotionValueEvent(scrollY, "change", (latest) => {
-    if (latest +2 > lastScrollY) {
+    if (latest +10 > lastScrollY) {
       setIsScrollingDown(true);
     } else {
       setIsScrollingDown(false);
